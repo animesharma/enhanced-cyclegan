7e01d9ce668ae7710e892b0ae5d9f84e51072022
Animesh Sharma <animesharma08+github@gmail.com>
2022-05-07 23:26:14 -0400

    Remove print statements

--- src/datasets.py
+++ src/datasets.py
@@ -10,10 +10,10 @@ class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
         self.transform = transforms.Compose(transforms_)
         self.unaligned = unaligned
-        print(os.path.join(root, '%s/A' % mode) + '/*.*')
+        #print(os.path.join(root, '%s/A' % mode) + '/*.*')
         self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
         self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
-        print(len(self.files_A), len(self.files_B))
+        #print(len(self.files_A), len(self.files_B))
 
     def __getitem__(self, index):
         item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]).convert('RGB'))
